70c8cdb7539ae04707e7c46a82cc6c19dd0ae681
mahadiduet <mahadiduet@gmail.com>
2024-02-24 21:14:04 +0600

    code push

--- scripts/script.js
+++ scripts/script.js
@@ -54,7 +54,7 @@ function bookingSeat(){
     addSeat.innerText="";
     bookingllSeat.forEach(element => { 
         var i = 0;     
-        console.log(element.innerText);
+        // console.log(element.innerText);
         const addElement = document.createElement('p');
         addElement.innerHTML = `<div class="flex justify-between">
         <h1>`+element.innerText+`</h1>
@@ -63,7 +63,7 @@ function bookingSeat(){
     </div>`;
         addSeat.appendChild(addElement);
     });
-    console.log(bookingllSeat);
+    // console.log(bookingllSeat);
 }
 
 function calculation(){
@@ -75,4 +75,48 @@ function calculation(){
     const priceElement = document.getElementById('total-price');
     priceElement.innerText = totalPrice;
     // console.log(totalPrice);
+    const grandElement = document.getElementById('grand-total');
+    grandElement.innerText = totalPrice;
+}
+
+function cuponApply(){
+    const selectesSeat = document.getElementById('selected-seat');
+    var selected = selectesSeat.innerText;
+    selected = parseInt(selected);
+    // console.log(selected);
+    const cuponCode = document.getElementById('coupon').value;
+    const coupon = cuponCode.toUpperCase();
+
+    // console.log(coupon)
+    const selectedSeat = document.getElementById('selected-seat');
+    var selecte = selectedSeat.innerText;
+    selecte = parseInt(selecte);
+    // // console.log(available)
+    const totalPrice = selecte * 550;
+    
+
+    if(selected === 4){
+        if(coupon === 'NEWS15'){
+            const grandTotal = totalPrice - ((totalPrice*15)/100);
+            const grandElement = document.getElementById('grand-total');
+            grandElement.innerText = grandTotal;
+        }
+        else if(coupon === 'COUPLE20'){
+            const grandTotal = totalPrice - ((totalPrice*20)/100);
+            const grandElement = document.getElementById('grand-total');
+            grandElement.innerText = grandTotal;
+        }
+        else{
+            const error = document.getElementById('error-coupon');
+            error.innerText = "Coupon invalid";
+        }
+    }
+    else{
+        const error = document.getElementById('error-coupon');
+        error.innerText = "You are not buy four seat.";
+    }
+}
+
+function next(){
+    console.log()
 }
